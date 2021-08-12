c63e8834ead53dff734867927831620c171699c1
AriGute <arielguata@gmail.com>
2021-08-12 22:14:53 +0300

    Tutorial part 30

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
 
-var server = http.createServer(function(req, res){
-    console.log('request was made: ' + req.url);
-    if(req.url === '/home' || req.url === '/'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/index.html').pipe(res);
-    } else if(req.url === '/contact'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname+'/contact.html').pipe(res);
-    }else{
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname+'/404.html').pipe(res);
-    }
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+app.get('/contact', function (req, res) {
+    res.render('contact');
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('yo dawgs, now listening to port 3K');
+
+
+app.get('/profile/:name', function (req, res) {
+    var data = {
+        age: 28,
+        job: 'web ninja',
+        hobbies: ['play games', 'eating', 'eating again']
+    }
+    res.render('profile', {
+        person: req.params.name,
+        data: data
+    });
+});
+
+
+
+app.listen(3000);
